67aa83a97bdead93460df2749b965bc6dde9ddcc
Khashayar Shomali <khashi792017@gmail.com>
2023-02-04 16:25:30 +0330

    Write codes to make a search bar.

---
+++ app.js
@@ -0,0 +1,38 @@
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[data-user-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+
+/*
+Loader
+*/
+
+const spinner = document.querySelector(".spinner");
+
+let users = [];
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  users.forEach((user) => {
+    const isVisible =
+      user.name.toLowerCase().includes(value) ||
+      user.email.toLowerCase().includes(value);
+    user.element.classList.toggle("hide", !isVisible);
+  });
+});
+
+spinner.removeAttribute("hidden");
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => res.json())
+  .then((data) => {
+    spinner.setAttribute("hidden", "");
+    users = data.map((user) => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      header.textContent = user.name;
+      body.textContent = user.email;
+      userCardContainer.append(card);
+      return { name: user.name, email: user.email, element: card };
+    });
+  });
